369e25786075c031f57de7734bf69f934577e8f5
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2019-12-22 10:22:25 +0900

    user登録後メール送信、非同期
    後々にできるようにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :trackable and :omniauthable
+  #  and :omniauthable :trackable,
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,14 @@ Rails.application.configure do
     host: 'localhost',
     port: 3000
   }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.secrets.SMTP_EMAIL,
+    password: Rails.application.secrets.SMTP_PASSWORD
+  }
 
   config.action_mailer.perform_caching = false
 
