0acfc9dec2d84a3fff048a74690ff9c8433eaea9
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2019-12-23 16:44:45 +0900

    ページネーションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  get 'posts/create'
   devise_for :users
+  
+  
   root 'home#index'
-  mount Sidekiq::Web, at: '/sidekiq'
+  resources :posts, only: [:create]
+
+  authenticate :user, ->(u) { u.admin? } do
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 end
