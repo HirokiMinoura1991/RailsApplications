27286b341368d3581189f3b7796160a8d10b04de
HirokiMinoura1991 <hirokiminoura.tech@gmail.com>
2020-01-01 12:55:54 +0900

    confirmation tokenの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_085345) do
+ActiveRecord::Schema.define(version: 2020_01_01_035315) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_085345) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
@@ -43,7 +42,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_085345) do
     t.datetime "avatar_updated_at"
     t.integer "posts_count", default: 0, null: false
     t.integer "role", default: 0, null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
